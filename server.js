b3a95b53c81b9478378d810fd07aab9d3e7a4194
emanuel caro <darkemlord@gmail.com>
2022-01-07 10:32:11 +0900

    app connected with shopify success

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const next = require('next');
 const { default: createShopifyAuth } = require('@shopify/koa-shopify-auth');
 const { verifyRequest } = require('@shopify/koa-shopify-auth');
 const session = require('koa-session');
-const { Router } = require('next/router');
+// const { Router } = require('next/router');
 
 dotenv.config();
 
