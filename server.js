099bca9ab07355be5e809ca13ce3ed1d201e96c5
emanuel caro <darkemlord@gmail.com>
2022-01-07 10:07:33 +0900

    server working

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const next = require('next');
 const { default: createShopifyAuth } = require('@shopify/koa-shopify-auth');
 const { verifyRequest } = require('@shopify/koa-shopify-auth');
 const session = require('koa-session');
+const { Router } = require('next/router');
 
 dotenv.config();
 
@@ -15,4 +16,35 @@ const handle = app.getRequestHandler();
 
 const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY } = process.env;
 
-console.log(process.env)
+app.prepare().then(() => {
+  const server = new Koa();
+  // const router = new Router();
+
+  server.use(session({secure:true, sameSite: 'none'}, server));
+  server.keys = [SHOPIFY_API_SECRET_KEY];
+
+  server.use(
+    createShopifyAuth(
+      {
+        apiKey: SHOPIFY_API_KEY,
+        secret: SHOPIFY_API_SECRET_KEY,
+        scopes: ['read_products'],
+        async afterAuth(ctx) {
+          const { shop, accesToken } = ctx.session;
+          ctx.redirect('/');
+        }
+      }
+    )
+  );
+
+  server.use(verifyRequest());
+  server.use(async (ctx) => {
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+    ctx.res.statusCode = 200;
+  });
+
+  server.listen(port, () => {
+    console.log(`Ready on http://localhost:${port}`)
+  })
+});
